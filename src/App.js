ae7fef8b8deebdf73ec9f766d4372b22be3eb138
Pigrabbit <donghyuk1003@gmail.com>
2020-08-17 22:11:14 +0900

    Refactor multiple useState into useReducer in order to focus on business logic

--- src/App.js
+++ src/App.js
@@ -1,29 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import './App.css';
 import { login } from './utils'
 
+
+const loginReducer = (state, action) => {
+  switch (action.type) {
+    case 'field': {
+      return {
+        ...state,
+        [action.field]: action.value
+      }
+    }
+    case 'login': {
+      return {
+        ...state,
+        isLoading: true,
+        error: ''
+      }
+    }
+    case 'success': {
+      return {
+        ...state,
+        isLoggedIn: true,
+        isLoading: false
+      }
+    }
+    case 'error': {
+      return {
+        ...state,
+        error: 'Incorrect username or password',
+        isLoading: false,
+        password: ''
+      }
+    }
+    case 'logout': {
+      return {
+        ...state,
+        isLoggedIn: false,
+        username: '',
+        password: ''
+      }
+    }
+
+    default:
+      break
+  }
+
+  return state
+}
+
+const initialState = {
+  username: '',
+  password: '',
+  isLoading: false,
+  error: '',
+  isLoggedIn: false
+}
+
 export const App = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState('')
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [state, dispatch] = useReducer(loginReducer, initialState)
+
+  const { username, password, isLoading, error, isLoggedIn } = state
 
   const onSubmit = async (e) => {
     e.preventDefault()
-
-    setIsLoading(true)
+    dispatch({ type: 'login' })
 
     try {
       await login({ username, password })
-      setIsLoggedIn(true)
-      setPassword('')
-      setError('')
+      dispatch({ type: 'success'})
     } catch (error) {
-      setError('Incorrect username or password')
+      dispatch({ type: 'error' })
     }
-
-    setIsLoading(false)
   }
 
   return (
@@ -32,7 +80,7 @@ export const App = () => {
         {isLoggedIn ? 
         <>
           <h1>Hello {username}!</h1>
-          <button className="logout-btn" onClick={() => setIsLoggedIn(false)}>Log Out</button>
+          <button className="logout-btn" onClick={() => dispatch({ type: 'logout' })}>Log Out</button>
         </> :
         <form className="form" onSubmit={onSubmit}>
           {error && <p className="error">{error}</p>}
@@ -41,14 +89,14 @@ export const App = () => {
             type="text" 
             placeholder="username"
             value={username}
-            onChange={e => setUsername(e.currentTarget.value)}
+            onChange={e => dispatch({ type: 'field', field: 'username', value: e.currentTarget.value})}
             />
           <input 
             type="password" 
             placeholder="password" 
             autoComplete="new-password"
             value={password}
-            onChange={e => setPassword(e.currentTarget.value)}
+            onChange={e => dispatch({ type: 'field', field: 'password', value: e.currentTarget.value})}
             />
           <button className="submit" type="submit" disabled={isLoading}>
             {isLoading? 'Loggin in...'  : 'Log In'}
