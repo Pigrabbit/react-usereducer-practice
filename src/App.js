01e076042703bb5a9bee3e8bb8eb85d815b370e8
Pigrabbit <donghyuk1003@gmail.com>
2020-08-17 22:49:33 +0900

    Refactor Login Page with react router

--- src/App.js
+++ src/App.js
@@ -1,108 +1,23 @@
-import React, { useState, useReducer } from 'react';
+import React from 'react';
 import './App.css';
-import { login } from './utils'
+import { LoginPage } from './LoginPage';
 
-
-const loginReducer = (state, action) => {
-  switch (action.type) {
-    case 'field': {
-      return {
-        ...state,
-        [action.field]: action.value
-      }
-    }
-    case 'login': {
-      return {
-        ...state,
-        isLoading: true,
-        error: ''
-      }
-    }
-    case 'success': {
-      return {
-        ...state,
-        isLoggedIn: true,
-        isLoading: false
-      }
-    }
-    case 'error': {
-      return {
-        ...state,
-        error: 'Incorrect username or password',
-        isLoading: false,
-        password: ''
-      }
-    }
-    case 'logout': {
-      return {
-        ...state,
-        isLoggedIn: false,
-        username: '',
-        password: ''
-      }
-    }
-
-    default:
-      break
-  }
-
-  return state
-}
-
-const initialState = {
-  username: '',
-  password: '',
-  isLoading: false,
-  error: '',
-  isLoggedIn: false
-}
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 export const App = () => {
-  const [state, dispatch] = useReducer(loginReducer, initialState)
-
-  const { username, password, isLoading, error, isLoggedIn } = state
-
-  const onSubmit = async (e) => {
-    e.preventDefault()
-    dispatch({ type: 'login' })
-
-    try {
-      await login({ username, password })
-      dispatch({ type: 'success'})
-    } catch (error) {
-      dispatch({ type: 'error' })
-    }
-  }
-
+  
   return (
     <div className="App">
-      <div className="login-container">
-        {isLoggedIn ? 
-        <>
-          <h1>Hello {username}!</h1>
-          <button className="logout-btn" onClick={() => dispatch({ type: 'logout' })}>Log Out</button>
-        </> :
-        <form className="form" onSubmit={onSubmit}>
-          {error && <p className="error">{error}</p>}
-          <p>Please Login</p>
-          <input 
-            type="text" 
-            placeholder="username"
-            value={username}
-            onChange={e => dispatch({ type: 'field', field: 'username', value: e.currentTarget.value})}
-            />
-          <input 
-            type="password" 
-            placeholder="password" 
-            autoComplete="new-password"
-            value={password}
-            onChange={e => dispatch({ type: 'field', field: 'password', value: e.currentTarget.value})}
-            />
-          <button className="submit" type="submit" disabled={isLoading}>
-            {isLoading? 'Loggin in...'  : 'Log In'}
-          </button>
-        </form>}
-      </div>
+      <Router>
+        <Switch>
+          <Route path='/login'>
+            <LoginPage />
+          </Route>
+          <Route path='/'>
+            <LoginPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
